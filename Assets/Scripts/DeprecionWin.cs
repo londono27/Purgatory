be4b5095cec1df988b25e68fa3510e72ddc6c87d
Daniel M <dmcuestaf@udistrital.edu.co>
2023-12-06 08:31:33 -0500

    cambiosD

--- Assets/Scripts/DeprecionWin.cs
+++ Assets/Scripts/DeprecionWin.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 
 public class DeprecionWin : MonoBehaviour
 {
-    public PlayableDirector director;
-
-
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             // Activar el Tilemap Renderer si el jugador entra en contacto
-            director.Play();
+            
         }
     }
 }
