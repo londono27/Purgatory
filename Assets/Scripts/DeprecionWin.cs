b82bd7cba231c89c639e1bb265f00c5e3d837a88
Destilou <dmcuestaf@correo.udistrital.edu.co>
2023-12-06 08:14:11 -0500

    se fue el deprecion nivel

---
+++ Assets/Scripts/DeprecionWin.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeprecionWin : MonoBehaviour
+{
+    public PlayableDirector director;
+
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Activar el Tilemap Renderer si el jugador entra en contacto
+            director.Play();
+        }
+    }
+}
